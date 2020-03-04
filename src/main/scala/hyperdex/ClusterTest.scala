f518ae19514bd0929062fdfce9bbe07d3fd1ba57
jannes <jannes.dev@posteo.net>
2020-03-04 12:08:41 +0100

    fix cluster test, add scalafmt conf

--- src/main/scala/hyperdex/ClusterTest.scala
+++ src/main/scala/hyperdex/ClusterTest.scala
@@ -8,21 +8,17 @@ import com.typesafe.config.ConfigFactory
 object ClusterTest {
 
   sealed trait Role
-  case object GatewayHiSayer extends Role
-  case object HiSayer extends Role
-  case object HiReceiver extends Role
+  case object GatewayNodeRole extends Role
+  case object DataNodeRole extends Role
 
   object RootBehavior {
     def apply(): Behavior[Nothing] = Behaviors.setup[Nothing] { context =>
       val cluster = Cluster(context.system)
       // determine own role
-      if (cluster.selfMember.hasRole(HiReceiver.toString)) {
+      if (cluster.selfMember.hasRole(DataNodeRole.toString)) {
         println("I am receiver")
         context.spawn(DataNode(), "receiverNode")
-      } else if (cluster.selfMember.hasRole(HiSayer.toString)) {
-        println("I am sender")
-        context.spawn(SenderNode(), "senderNode")
-      } else if (cluster.selfMember.hasRole(GatewayHiSayer.toString)) {
+      } else if (cluster.selfMember.hasRole(GatewayNodeRole.toString)) {
         println("I am gateway sender")
         context.spawn(GatewayNode("localhost", 8080), "gatewayNode")
       } else {
@@ -35,12 +31,10 @@ object ClusterTest {
   def main(args: Array[String]): Unit = {
     require(args.length == 2, "Usage: role port")
     args(0) match {
-      case "hisayer" =>
-        startup(HiSayer, args(1).toInt)
-      case "hireceiver" =>
-        startup(HiReceiver, args(1).toInt)
+      case "data" =>
+        startup(DataNodeRole, args(1).toInt)
       case "gateway" =>
-        startup(GatewayHiSayer, args(1).toInt)
+        startup(GatewayNodeRole, args(1).toInt)
       case _ =>
         println("supplied wrong role")
         System.exit(1)
