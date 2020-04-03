452ccb9cf2065767adc84b0d010f808ce562833d
Emiel Bos <emielboss@gmail.com>
2020-04-04 01:06:29 +0200

    Started GatlingSeachTest

--- src/test/scala/hyperdex/GatlingGetTest.scala
+++ src/test/scala/hyperdex/GatlingGetTest.scala
@@ -13,45 +13,38 @@ class GatlingGetTest extends Simulation {
   val indexFeeder = Iterator.from(1).map(i => Map("index" -> i))
   val attributeFeeder = Iterator.continually(Map("attribute1" -> Random.nextInt(100).toString, "attribute2" -> Random.nextInt(100).toString))
 
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+
   val putRecord = feed(indexFeeder).feed(attributeFeeder).exec(http("post")
     .post("/put/table/${index}")
     .header("Content-Type", "application/json")
     .body(StringBody("""{ "attribute1" : ${attribute1}, "attribute2" : ${attribute2} }""")).check(status.is(200)))
-
-
-  val httpProtocol = http
-    .baseUrl("http://localhost:8080")
 
   val createTable = http("post")
     .post("/create/table")
     .header("Content-Type", "application/json")
     .body(StringBody("[\"attribute1\", \"attribute2\"]"))
 
-    val getTable = http("get")
+    val getRecord = http("get")
      .get("/get/table/${randomIndex}")
     .check(status.is(200)).check(bodyString.exists)
 
-
-
-  val scn = scenario(s" ${amountOfPutRequests} put")
+  val scnPut = scenario(s" ${amountOfPutRequests} put")
     .exec(createTable)
     .repeat(amountOfPutRequests){
       exec(putRecord)
     }
 
-
-  val scn2 = scenario(s"Test get after ${amountOfPutRequests} put")
-  repeat(amountOfPutRequests/2){
+  val scnGet = scenario(s"Test get after ${amountOfPutRequests} put")
+  .repeat(amountOfPutRequests/2){
     feed(randomIndexFeeder)
-      .exec(getTable)
+      .exec(getRecord)
   }
 
-
   setUp(
-    scn.inject(atOnceUsers(1)),
-    scn2.inject(atOnceUsers(1))
-
+    scnPut.inject(atOnceUsers(1)),
+    scnGet.inject(atOnceUsers(1))
   ).protocols(httpProtocol)
-
 
 }
