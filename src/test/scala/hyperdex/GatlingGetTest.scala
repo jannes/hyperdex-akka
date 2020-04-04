60e28e14923ca81913f193e6ca1a4e32226c0ee7
Tomashh94 <tomashh94@gmail.com>
2020-04-04 21:44:26 +0200

    Small fix so the get index is random and accurate

--- src/test/scala/hyperdex/GatlingGetTest.scala
+++ src/test/scala/hyperdex/GatlingGetTest.scala
@@ -1,50 +1,52 @@
-//package hyperdex
-//
-//import io.gatling.core.Predef._
-//import io.gatling.http.Predef._
-//
-//import scala.concurrent.duration._
-//import scala.util.Random
-//
-//class GatlingGetTest extends Simulation {
-//  val amountOfPutRequests= 100000
-//
-//  val randomIndexFeeder = Iterator.continually(Map("randomIndex" -> Random.nextInt(amountOfPutRequests).toString))
-//  val indexFeeder = Iterator.from(1).map(i => Map("index" -> i))
-//  val attributeFeeder = Iterator.continually(Map("attribute1" -> Random.nextInt(100).toString, "attribute2" -> Random.nextInt(100).toString))
-//
-//  val httpProtocol = http
-//    .baseUrl("http://localhost:8080")
-//
-//  val putRecord = feed(indexFeeder).feed(attributeFeeder).exec(http("post")
-//    .post("/put/table/${index}")
-//    .header("Content-Type", "application/json")
-//    .body(StringBody("""{ "attribute1" : ${attribute1}, "attribute2" : ${attribute2} }""")).check(status.is(200)))
-//
-//  val createTable = http("post")
-//    .post("/create/table")
-//    .header("Content-Type", "application/json")
-//    .body(StringBody("[\"attribute1\", \"attribute2\"]"))
-//
-//    val getRecord = http("get")
-//     .get("/get/table/${randomIndex}")
-//    .check(status.is(200)).check(bodyString.exists)
-//
-//  val scnPut = scenario(s" ${amountOfPutRequests} put")
-//    .exec(createTable)
-//    .repeat(amountOfPutRequests){
-//      exec(putRecord)
-//    }
-//
-//  val scnGet = scenario(s"Test get after ${amountOfPutRequests} put")
-//  .repeat(amountOfPutRequests/2){
-//    feed(randomIndexFeeder)
-//      .exec(getRecord)
-//  }
-//
-//  setUp(
-//    scnPut.inject(atOnceUsers(1)),
-//    scnGet.inject(atOnceUsers(1))
-//  ).protocols(httpProtocol)
-//
-//}
+package hyperdex
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+import scala.util.Random
+
+class GatlingGetTest extends Simulation {
+
+
+  val indexFeeder: Iterator[Map[String, Int]] = Iterator.from(1).map(i => Map("index" -> i))
+  val attributeFeeder: Iterator[Map[String, String]] = Iterator.continually(Map("attribute1" -> Random.nextInt(100).toString, "attribute2" -> Random.nextInt(100).toString))
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+
+  val putRecord = feed(indexFeeder).feed(attributeFeeder).exec(http("post")
+    .post("/put/table/${index}")
+    .header("Content-Type", "application/json")
+    .body(StringBody("""{ "attribute1" : ${attribute1}, "attribute2" : ${attribute2} }""")).check(status.is(200)))
+
+  val createTable = http("post")
+    .post("/create/table")
+    .header("Content-Type", "application/json")
+    .body(StringBody("[\"attribute1\", \"attribute2\"]"))
+
+    val getRecord = exec(http("get")
+     .get("/get/table/${randomIndex}")
+    .check(status.is(200)).check(bodyString.exists))
+
+  def newFeeder(): Iterator[Map[String, String]] ={
+    val maxIndex = "${n}".toInt * "${numRecords}".toInt
+    Iterator.continually(Map("randomIndex" -> Random.nextInt(maxIndex).toString))
+  }
+
+
+  val scn = scenario(" Experiment put")
+    .exec(createTable)
+    .repeat(10, "n"){
+      exec(repeat(10000, "numRecords"){
+        exec(putRecord)
+      })
+      feed(newFeeder()).exec(getRecord)
+    }
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+
+  ).protocols(httpProtocol)
+
+}
