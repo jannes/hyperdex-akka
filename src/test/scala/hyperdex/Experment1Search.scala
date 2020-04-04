d81ddb64a6357a133c804feddd5f7e7c15b9bf0a
Emiel Bos <emielboss@gmail.com>
2020-04-04 21:16:03 +0200

    Forget to add -_-

---
+++ src/test/scala/hyperdex/Experment1Search.scala
@@ -0,0 +1,73 @@
+package hyperdex
+
+import io.gatling.core.Predef._
+import io.gatling.core.body.StringBody
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+import scala.util.Random
+
+class Experiment1Search extends Simulation {
+
+  val NUM_ATTRIBUTES = 5
+
+  val indexFeeder = Iterator.from(1).map(i => Map("index" -> i))
+
+  val valueFeeder = Iterator.from(1).map(i => Map("value" -> i))
+
+  def generateTableString(numAttributes: Int): StringBody = {
+    var generateTableString: String = "[\"attribute1\""
+    for(attribute <- 2 to numAttributes){
+      generateTableString = generateTableString.concat(s""", "attribute$attribute"""")
+    }
+    generateTableString = generateTableString.concat("]")
+    StringBody(generateTableString)
+  }
+
+  def generatePutString(numAttributes: Int): StringBody = {
+    var putString: String = s"""{"attribute1" : ${1}"""
+    for(attribute <- 2 to numAttributes){
+      putString = putString.concat(s""", "attribute$attribute" : ${Random.nextInt(100)}""")
+    }
+    putString = putString.concat("}")
+
+    StringBody(putString)
+  }
+
+  def generateSearch(numAttributes: Int): HttpRequestBuilder = {
+    val searchRecord = http("Search among ${n} * 10000 records")
+      .get(url="/search/table")
+      .header("Content-Type", "application/json")
+      .body(generatePutString(numAttributes))
+      .check(status is 200)
+    searchRecord
+  }
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+
+  val createTable = http("createTable")
+    .post("/create/table")
+    .header("Content-Type", "application/json")
+    .body(generateTableString(NUM_ATTRIBUTES))
+    .check(bodyString is "Create successful")
+
+  val putRecord = feed(indexFeeder).exec(http("putRecord")
+    .post(url="/put/table/${numRecords}") // n is provided by loop in the scenario
+    .header("Content-Type", "application/json")
+    .body(generatePutString(NUM_ATTRIBUTES))
+    .check(bodyString is "Put Succeeded"))
+
+  val scn = scenario("Experiment 1: Search")
+    .exec(createTable)
+    .repeat(10, "n"){
+      exec(repeat(10000, "numRecords"){
+          exec(putRecord)
+      })
+      .exec(generateSearch(NUM_ATTRIBUTES))
+    }
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
+
+}
