c4f051535c538f8de0d40faf742d2712198f0917
jannes <jannes.dev@posteo.net>
2020-04-05 14:06:36 +0200

    add script for running experiment 1

--- experiment2.sh
+++ experiment2.sh
@@ -1,11 +1,9 @@
-# amounts=(1,2,4,8,16)
-amounts=(1)
+amounts=(1 2 4 8 16)
 for ((i = 0 ; i < ${#amounts[@]}; i++)); do
     amount=${amounts[i]}
     export NUM_DATANODES=$amount 
-    echo $amount
     docker-compose up --scale datanode=$NUM_DATANODES -d
-    sleep 10
+    sleep 20
     sbt "gatling:testOnly hyperdex.Experiment2" 
     docker-compose down
     mkdir "experiment2-${amount}n"
